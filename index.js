bbac6a1fe547607777b17906026a2feaa5f0f317
Lucas Tormin <lucasntormin@gmail.com>
2017-11-12 23:06:44 -0200

    Mongoose saving user

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+// Models
+require("./models/User");
+
+// Passport
 require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 require("./routes/authRoutes")(app);
