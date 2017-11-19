670d8265c3a8b3fce74029928f8130e44170023f
Lucas Tormin <lucasntormin@gmail.com>
2017-11-19 01:42:44 -0200

    Basic App routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>Survey new</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hey!</div>;
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
