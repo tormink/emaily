d73ca1a21a8c64bfefca8890ed1b15924f6a6abb
Lucas Tormin <lucasntormin@gmail.com>
2017-11-19 02:55:22 -0200

    Login and logout behavior

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class Header extends Component {
+class Header extends Component {
+  renderContent = () => {
+    const { auth } = this.props;
+    switch (auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  };
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Emaily</a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
+
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+};
+
+export default connect(mapStateToProps)(Header);
