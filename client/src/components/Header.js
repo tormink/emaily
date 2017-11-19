0455c736a3842d544ce7d97af6b32f4d51d7ef48
Lucas Tormin <lucasntormin@gmail.com>
2017-11-19 01:43:01 -0200

    Sample Header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">Emaily</a>
+          <ul className="right">
+            <li>
+              <a>Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
