ff1eab3b83814dfdf43d2de06ca837a3ce5341a3
Lucas Tormin <lucasntormin@gmail.com>
2017-11-20 00:53:58 -0200

    Frontend billing

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,3 +6,9 @@ export const fetchUser = () => async dispatch => {
 
   dispatch({ type: FETCH_USER, payload: data });
 };
+
+export const handleToken = token => async dispatch => {
+  const { data } = await axios.post("/api/stripe", token);
+
+  dispatch({ type: FETCH_USER, payload: data });
+};
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
+import Payments from "./Payments";
+
 class Header extends Component {
   renderContent = () => {
     const { auth } = this.props;
@@ -15,11 +17,14 @@ class Header extends Component {
           </li>
         );
       default:
-        return (
-          <li>
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="2">
             <a href="/api/logout">Logout</a>
           </li>
-        );
+        ];
     }
   };
   render() {
